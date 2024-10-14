61664e25bb9ec476e351068269a04728bafa5a79
CanCorduk <CanCorduk@CAN>
2024-10-14 22:11:10 +0300

    Proje dosyası ekle.

---
+++ VehicleMaintenance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleMaintenance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
