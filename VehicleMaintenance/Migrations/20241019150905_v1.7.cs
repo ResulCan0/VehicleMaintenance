af5ccf930cc49ca62451e1af252e3eec5018b30e
Aslan Can <aslancan974@icloud.com>
2024-10-19 18:10:06 +0300

    migration çözüldü.

---
+++ VehicleMaintenance/Migrations/20241019150905_v1.7.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleMaintenance.Migrations
+{
+    /// <inheritdoc />
+    public partial class v17 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CompanyUsers_Roles_RoleId",
+                table: "CompanyUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Roles",
+                table: "Roles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                table: "Roles",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompanyUsers_Roles_RoleId",
+                table: "CompanyUsers",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CompanyUsers_Roles_RoleId",
+                table: "CompanyUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                table: "Roles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Roles",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompanyUsers_Roles_RoleId",
+                table: "CompanyUsers",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
