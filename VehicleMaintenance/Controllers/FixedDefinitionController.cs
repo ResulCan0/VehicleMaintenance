86eadd179a128d8e82ff851a312a42db174523e8
Can Çördük <r.cancorduk@gmail.com>
2024-11-15 09:37:54 +0300

    Sabit tanımlar halledildi.

--- VehicleMaintenance/Controllers/FixedDefinitionController.cs
+++ VehicleMaintenance/Controllers/FixedDefinitionController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace VehicleMaintenance.Controllers
 {
@@ -41,6 +42,94 @@ namespace VehicleMaintenance.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(fixedDefinition);
+        }
+
+        // GET: FixedDefinition/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+           ViewData["FixedDefinitionId"] = new SelectList(
+          "FixedDefinitionId",
+          "GroupCode",
+          "Code",
+          "Name"
+           );
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var FixedDefinition = await _context.FixedDefinitions.FindAsync(id);
+            if (FixedDefinition == null)
+            {
+                return NotFound();
+            }
+            return View(FixedDefinition);
+        }
+
+        // GET: FixedDefinition/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("FixedDefinitionId,GroupCode,Code,Name")] FixedDefinition fixedDefinition)
+        {
+            if (id != fixedDefinition.FixedDefinitionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fixedDefinition);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FixedDefinitionExists(fixedDefinition.FixedDefinitionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fixedDefinition);
+        }
+        // POST: FixedDefinition/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fixedDefinition = await _context.FixedDefinitions
+                .FirstOrDefaultAsync(m => m.FixedDefinitionId == id);
+            if (fixedDefinition == null)
+            {
+                return NotFound();
+            }
+
+            return View(fixedDefinition);
+        }
+
+        // POST: FixedDefinition/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fixeddefinition = await _context.FixedDefinitions.FindAsync(id);
+            _context.FixedDefinitions.Remove(fixeddefinition);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool FixedDefinitionExists(Guid id)
+        {
+            return _context.FixedDefinitions.Any(e => e.FixedDefinitionId == id);
         }
     }
 }
